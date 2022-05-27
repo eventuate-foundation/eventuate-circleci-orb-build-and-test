7243ea23514dd456667ef784c4e9d1894f5e9e79
Chris Richardson <chris@chrisrichardson.net>
2022-05-27 13:48:37 +0900

    Trim spaces from JDK version

--- src/scripts/install-java-11.sh
+++ src/scripts/install-java-11.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
+export SDKMAN_DIR=/home/circleci/.sdkman
+
 curl -s "https://get.sdkman.io" | bash
 source "/home/circleci/.sdkman/bin/sdkman-init.sh"
-sdk install java "$(sdk list java | grep -E  ' 11.*zulu' | head -1 | cut -f6 -d\|)"
+sdk install java "$(sdk list java | grep -E  ' 11.*zulu' | head -1 | cut -f6 -d\| | awk '{$1=$1;print}')"
