702eaae3d98d8f7fcfb309d434c642d77586e897
Chris Richardson <chris@chrisrichardson.net>
2023-05-08 20:31:43 -0700

    Parameterize Java version to install

--- src/scripts/install-java-11.sh
+++ src/scripts/install-java-11.sh
@@ -4,4 +4,4 @@ export SDKMAN_DIR=/home/circleci/.sdkman
 
 curl -s "https://get.sdkman.io" | bash
 source "/home/circleci/.sdkman/bin/sdkman-init.sh"
-sdk install java "$(sdk list java | grep -E  ' 11.*zulu' | head -1 | cut -f6 -d\| | awk '{$1=$1;print}')"
+sdk install java "$(sdk list java | grep -E  " $JAVA_VERSION_TO_INSTALL[0-9.]*-zulu" | head -1 | cut -f6 -d\| | awk '{$1=$1;print}')"
